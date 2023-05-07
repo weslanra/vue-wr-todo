afb71746f1cb934c8e9370dd208d205eb9f88ed3
Weslan Alves <weslan.rezende@gmail.com>
2023-05-07 08:49:36 -0300

    feat(Arquitetura) Adicionar dependências do projeto
    - Adicionando vuexy;
    - Configurando projeto;

---
+++ src/navigation/horizontal/todo.ts
@@ -0,0 +1,11 @@
+export default [
+  {
+    title: 'Entrada',
+    icon: { icon: 'tabler-inbox' },
+    to: 'todo-inbox',
+  },
+  {
+    title: 'Hoje',
+    icon: { icon: 'tabler-calendar' },
+  },
+]
---
+++ src/navigation/vertical/index.ts
@@ -0,0 +1,10 @@
+import type { VerticalNavItems } from '@/@layouts/types'
+import appAndPages from './app-and-pages'
+import charts from './charts'
+import dashboard from './dashboard'
+import forms from './forms'
+import others from './others'
+import todo from './todo'
+import uiElements from './ui-elements'
+
+export default [...todo, ...dashboard, ...appAndPages, ...uiElements, ...forms, ...charts, ...others] as VerticalNavItems
